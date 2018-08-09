45e4f3f65d49541bf641febf8bfd1ee68e5679f4
Lewis Kim <lewis.k@berkeley.edu>
2018-08-09 12:18:18 -0700

    Finished integrating backend code for sentiment analysis.
    Results displayed on HTML templates without any design or UI/UX elements.

--- mysite/sentiments/tweets.py
+++ mysite/sentiments/tweets.py
@@ -110,3 +110,81 @@ def create_tweet_df(tweets,):
 def get_hour(day):
 	hour = day.hour + day.minute/60 + day.second/3600
 	return hour
+
+
+### Analysis functions. ###
+
+"""Return an overall sentiment (string) for a given polarity SCORE. Sentiments
+   range from very positive, generally positive, slightly positive, neutral,
+   slightly negative, generally negative, and very negative.
+   Scale may need readjustments."""
+def get_sentiment(score):
+	
+	# Ranges for score-to-sentiment scale, from the VADER Lexicon documentation.
+	# Negative ranges.
+	if score < -4:
+		return "extremely negative."
+
+	elif score >= -4 and score <= -3:
+		return "very negative."
+
+	elif score > -3 and score <= -2:
+		return "fairly negative."
+
+	elif score > -2 and score <= -1:
+		return "negative."
+
+	elif score > -1 and score < -0.1:
+		return "slightly negative."
+
+	# Neutral range.
+	elif score >= -0.1 and score <= 0.1:
+		return "neutral."
+
+	# Positive ranges.
+	elif score > 0.1 and score <= 1:
+		return "slightly positive."
+
+	elif score > 1 and score <= 2:
+		return "positive."
+
+	elif score > 2 and score <= 3:
+		return "fairly positive."
+
+	elif score > 3 and score <= 4:
+		return "very positive."
+
+	elif score > 4:
+		return "extremely positive."
+
+"""Return a list of tuples of top 10 most positive tweets and their dates, from the
+   selected user's Twitter DATA."""
+def get_positive_tweets(data):
+	
+	# Sort 'polarity' values in descending order.
+	df = data.sort_values('polarity', ascending = False)
+
+	tweets = df['text'].tolist()[0:10]
+	dates = df['time'].tolist()[0:10]
+	polarities = [round(x, 2) for x in df['polarity'].tolist()[0:10]]
+
+	pos_tweets = [(x, y, z) for x, y, z in zip(tweets, dates, polarities)]
+
+	return pos_tweets
+
+
+"""Return a list of tuples of top 10 most negative tweets and their dates, from the
+   selected user's Twitter DATA."""
+def get_negative_tweets(data):
+
+	# Sort 'polarity' values in ascending order.
+	df = data.sort_values('polarity', ascending = True)
+
+	tweets = df['text'].tolist()[0:10]
+	dates = df['time'].tolist()[0:10]
+	polarities = [round(x, 2) for x in df['polarity'].tolist()[0:10]]
+
+	neg_tweets = [(x, y, z) for x, y, z in zip(tweets, dates, polarities)]
+
+	return neg_tweets
+
--- mysite/sentiments/views.py
+++ mysite/sentiments/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+import statistics
 from . import tweets
+
 # Create your views here.
 
 def index(request):
@@ -10,8 +13,17 @@ def index(request):
 	twitter_data = tweets.get_twitter_data(username)
 	sentiment_data = tweets.create_tweet_df(twitter_data)
 
+	# sentiment_data columns: ['text' 'retweet_count' 'time' 'location' 'pst_time' 'hour' 'no_punc', 'polarity']
+
+	avg_polarity = round(statistics.mean(sentiment_data['polarity'].tolist()), 4)
+
+	top_10_pos = tweets.get_positive_tweets(sentiment_data)
+	top_10_neg = tweets.get_negative_tweets(sentiment_data)
+
 	# print(sentiment_data['polarity'].tolist())
 
 	# print(len(sentiment_data))
+
+	context = {"username": username, "avg_polarity": avg_polarity, "top_10_pos": top_10_pos, "top_10_neg": top_10_neg}
 
 	return render(request, "sentiments/analysis.html", context = context)
