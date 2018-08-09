572f4b12ed5790147adb702af1820d7032a65c87
Lewis Kim <lewis.k@berkeley.edu>
2018-08-09 10:28:44 -0700

    Added HTML templates for Sentiment Analysis.

--- mysite/sentiments/views.py
+++ mysite/sentiments/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . import tweets
 # Create your views here.
 
 def index(request):
-	return HttpResponse("<h2>HEY</h2>")
+	username = request.GET['username']
+	context = {'username': username}
+
+
+	return render(request, "sentiments/analysis.html", context = context)
