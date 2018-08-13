b33b39c5fdd80070413842b1ecab3bbf87120460
Lewis Kim <lewis.k@berkeley.edu>
2018-08-12 17:15:40 -0700

    Added new URLs to sentiments.urls, and implemented a graph page for the user being analyzed.

--- mysite/sentiments/views.py
+++ mysite/sentiments/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 
+import json
+import datetime
+
 import statistics
 from . import tweets
 # Create your views here.
@@ -32,30 +35,37 @@ def index(request):
 	top_10_pos = tweets.get_positive_tweets(sentiment_data)
 	top_10_neg = tweets.get_negative_tweets(sentiment_data)
 
-	# Remove the "_normal.jpg" tag at the end to get the full-size image.
 	img = tweets.get_user_img(username)['profile_image_url']
+	# Remove the "_normal.jpg" tag at the end to get the full-size image.
 	user_img = tweets.get_user_img(username)['profile_image_url'][0:len(img)-11] + ".jpg"
 
-	#
+	# Add a new column called "tweet_date" to sentiment_data that consists of all tweet dates, without specific times.
+	# (i.e. 3-14-2018 8:00 PM becomes 3-14-2018). Then group the sentiment_data by "tweet_date", and save to a new df.
 	sentiment_data['tweet_date'] = sentiment_data['pst_time'].dt.date
-	df = sentiment_data.groupby('tweet_date').sum().reset_index(drop = False)
+	df = sentiment_data.groupby('tweet_date').mean().reset_index(drop = False)
 
-	labels = df['tweet_date'].tolist()
-	values = df['polarity'].tolist()
-
-	request.session['sentiment_data'] = sentiment_data
+	request.session['df'] = df
 
 	context = {"username": username, "avg_polarity": avg_polarity, "sentiment": sentiment,
-		"top_10_pos": top_10_pos, "top_10_neg": top_10_neg,"user_img": user_img, "labels": labels, "values": values}
+		"top_10_pos": top_10_pos, "top_10_neg": top_10_neg,"user_img": user_img}
 
 	return render(request, "sentiments/analysis.html", context = context)
 
 
 # 
 def graphs(request):
-	print(request.session['sentiment_data'])
+	df = request.session['df']
 
-	return render(request, "sentiments/graphs.html")
+	avg_polarity = round(statistics.mean(df['polarity'].tolist()), 4)
+
+	values = [round(x, 2) for x in df['polarity'].tolist()]
+	labels = [x.isoformat() for x in df['tweet_date'].tolist()]
+
+	series = [[((datetime.datetime.strptime(x, '%Y-%m-%d'))-datetime.datetime(1970,1,1)).total_seconds()*1000, y] for x, y in zip(labels, values)]
+
+	context = {"data": json.dumps(series), "username": request.GET['username']}
+
+	return render(request, "sentiments/graphs.html", context)
 
 
 
